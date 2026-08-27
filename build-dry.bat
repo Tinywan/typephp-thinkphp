aaf0e1c2c4562288d2e241e8495b5f0e0c4d5e99
yangweijie <917647288@qq.com>
2026-08-27 08:39:03 +0800

    删除无效bat

--- build-dry.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /d C:\git\source\tpc_v1095_windows_x86_64
-tpc.exe C:\git\php\typephp-think\project.yml --dry --no-progress 2>&1
-pause
